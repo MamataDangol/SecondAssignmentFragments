75efc7caa5dfe9ea9e4dc601cc239cfe511ffafa
MamataDangol <Mamatadangol3@gmail.com>
2019-12-10 17:56:02 +0545

    Area of Circle, Simple Interest, Automorphic, palindrome and Armstrong Calculated Succsfully.

--- app/src/main/java/com/mamata/secondassignmentfragments/Fragments/SwappingFragment.java
+++ app/src/main/java/com/mamata/secondassignmentfragments/Fragments/SwappingFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mamata.secondassignmentfragments.R;
 
@@ -15,6 +18,11 @@ import com.mamata.secondassignmentfragments.R;
  * A simple {@link Fragment} subclass.
  */
 public class SwappingFragment extends Fragment implements View.OnClickListener{
+    private EditText etfirst, etsecond;
+    private Button btncalc;
+    private TextView tvSwapResult;
+
+
 
 
     public SwappingFragment() {
@@ -26,11 +34,30 @@ public class SwappingFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swapping, container, false);
+        View view = inflater.inflate(R.layout.fragment_swapping, container, false);
+        etfirst = view.findViewById(R.id.etfirst);
+        etsecond = view.findViewById(R.id.etsecond);
+        btncalc = view.findViewById(R.id.btncalc);
+        btncalc.setOnClickListener(this);
+        return view;
+
     }
 
     @Override
     public void onClick(View v) {
+        int first, second, temp, bfirst, bsecond;
+        first = Integer.parseInt(etfirst.getText().toString());
+        second = Integer.parseInt(etsecond.getText().toString());
+        bfirst = first;
+        bsecond = second;
+
+        temp = first;
+        first = second;
+        second = temp;
+
+
+        tvSwapResult.append("the value before swipping. \n First Number is " + bfirst + "\n Second Number" +
+                bsecond + ".\n The value after swipping.\n First number is " +first + "\n Second Number is" +second + ".\n");
 
     }
 }
